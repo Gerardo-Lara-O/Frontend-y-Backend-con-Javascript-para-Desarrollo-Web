4315d06e86824e1290bb8afa18a02c057bc45bc2
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-08-18 01:51:53 -0600

    Creacion de Strings

--- 30 - Curso de Fundamentos de JavaScript/README.md
+++ 30 - Curso de Fundamentos de JavaScript/README.md
@@ -2,3 +2,4 @@
 - 01 - hola-mundo.js
 - 02 - clase-variables.js
 - 03 - tipoDeDatos.js
+- 04 - strings.js
---
+++ 30 - Curso de Fundamentos de JavaScript/strings.js
@@ -0,0 +1,13 @@
+// Tipos de strings
+
+let string1 = 'Hola mundo';
+let string2 = "Java Script es genial";
+let string3 = `${string1} feliz :)`
+// console.log(string3);
+let string4 = string1 + string2;
+let frase = "JavaScript es Extremadamente Genial"
+console.log(frase.length);
+console.log(frase.toLocaleLowerCase());
+
+console.log(frase.substring(0,10));
+
