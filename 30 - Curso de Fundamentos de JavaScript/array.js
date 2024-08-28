e6bd6d0060f95eb1d5bf4f1c6e8f562181f6162f
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-08-28 14:24:43 -0600

    Mutabilidad e Inmutabilidad de un array

--- 30 - Curso de Fundamentos de JavaScript/array.js
+++ 30 - Curso de Fundamentos de JavaScript/array.js
@@ -1,3 +1,25 @@
 let numeros = [1, 2, 3, 4, 5];  // Array de números
 let frutas = ["manzana", "naranja", "pera"];  // Array de cadenas
 let mixto = [1, "dos", { tres: 3 }, [4, 5]];  // Array mixto
+
+// Metodos Mutables
+frutas.push('Sandia')
+console.log(frutas);
+
+
+// Metodos inmutables
+let nuevosNumeros = numeros.concat([6, 7]);
+console.log(numeros);        // [1, 2, 3 ,4 ,5]
+console.log(nuevosNumeros);  // [1, 2, 3, 4, 5, 6, 7]
+
+let isArray = Array.isArray(frutas)
+console.log(isArray);
+
+
+// Ejercicio: Sumar todos los elementos de un array
+const numberArray = [1,2,3,4,5]
+let sum = 0
+for(let i = 0; i < numberArray.length; i++){
+    sum += numberArray[i]
+}
+console.log(sum);
