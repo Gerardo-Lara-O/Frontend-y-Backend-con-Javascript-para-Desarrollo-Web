2b9738a1741f8d22e23e0f378354dbff4b586cb2
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-08-29 00:20:02 -0600

    Metodos forEach() y map()

--- 30 - Curso de Fundamentos de JavaScript/array.js
+++ 30 - Curso de Fundamentos de JavaScript/array.js
@@ -23,3 +23,30 @@ for(let i = 0; i < numberArray.length; i++){
     sum += numberArray[i]
 }
 console.log(sum);
+
+// Ejemplo con foreach()
+const colors =['red', 'pink', 'blue']
+const iteratedColors = colors.forEach(color => console.log(color))
+console.log(colors);
+console.log(iteratedColors);
+
+
+// ejercicio fahrenheit to celsius conversion
+
+const temperatureInFahrenheit = [32,68,95,104,212]
+const temperatureInCelsius = temperatureInFahrenheit.map(fahrenheit => (5/9) * (fahrenheit - 32))
+
+console.log(`Temperatures In Fahrenheit ${temperatureInFahrenheit}`);
+console.log(`Temperatures In Celcius ${temperatureInCelsius}`);
+
+
+// Sum of Elements in Array
+let newNumbers = [1, 2, 3, 4, 5];
+let suma = 0
+newNumbers.forEach(element => {
+    suma += element
+});
+
+console.log(`La suma de los numeros ${newNumbers} es: ${suma}`);
+
+
