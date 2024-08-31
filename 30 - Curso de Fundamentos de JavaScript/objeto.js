ffd53285a961b6966ac7307f70e9925f9c0dd31d
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-08-30 20:12:19 -0600

    Anatomia de un objeto

---
+++ 30 - Curso de Fundamentos de JavaScript/objeto.js
@@ -0,0 +1,16 @@
+let persona = {
+    nombre : 'Gerardo',
+    apellido : 'Lara',
+    edad : 27,
+    fechaNacimiento: '27/01/1997',
+    direccion : {
+        calle : 'xxx',
+        ciudad : 'Mexico'
+    },
+    saludar : function(){
+        console.log(`Hola mi nombre es ${this.nombre}`);
+        
+    }
+}
+
+persona.saludar()
