d78b3a49a0efbe266388665b1b3db97260648bf9
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-09-05 15:44:50 -0600

    Metodo Get

--- 30 - Curso de Fundamentos de JavaScript/app.js
+++ 30 - Curso de Fundamentos de JavaScript/app.js
@@ -1,18 +1,49 @@
-const urls = [
-    "https://rickandmortyapi.com/api/character",
-    "https://rickandmortyapi.com/api/location",
-    "https://rickandmortyapi.com/api/episode"
-];
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
 
-async function fetchNewData(){ // Vamos empezar con una funcion asincronica
-    try{
-        for await (let url of urls){ // por cada url que exista en urls, haz lo siguiente
-            let response = await fetch(url); // Nos va a traer una url de este array
-            let data = await response.json(); 
-            console.log(data);
-        }
-    }
-    catch(error){
-        console.log(error);
+function sendHTTPRequest(method, url, data) {
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }).then((response) => {
+        return response.json();
+    });
+}
+
+async function fecthPosts() {
+    const responseData = await sendHTTPRequest(
+        "GET",
+        "https://jsonplaceholder.typicode.com/posts"
+    );
+    console.log(responseData);
+    const listOfPosts = responseData;
+
+    for (const post of listOfPosts) {
+        const postContainer = document.createElement("article");
+        postContainer.id = post.id;
+        postContainer.classList.add("post-item");
+
+        const title = document.createElement("h2");
+        title.textContent = post.title;
+
+        const body = document.createElement("p");
+        body.textContent = post.body;
+
+        const button = document.createElement("button");
+        button.textContent = "DELETE Content";
+
+        postContainer.append(title);
+        postContainer.append(body);
+        postContainer.append(button);
+
+        listElement.append(postContainer);
     }
 }
+
+fetchButton.addEventListener("click", fecthPosts);
