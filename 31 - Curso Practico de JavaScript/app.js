fe0a49d03a2fa56df009cda1ecd2bd4b04e821db
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-09-06 20:03:31 -0600

    addEventListener

--- 31 - Curso Practico de JavaScript/app.js
+++ 31 - Curso Practico de JavaScript/app.js
@@ -1,38 +1,13 @@
 let h1 = document.querySelector('h1')
-let p = document.querySelector('p')
-let parrafito = document.querySelector('.parrafito')
-let pid = document.querySelector('#pid')
-let input = document.querySelector('input')
-let div = document.querySelector('div')
+let input1 = document.querySelector('#calculo1')
+let input2 = document.querySelector('#calculo2')
+let btnCalcular = document.querySelector('#btnCalcular')
+let result = document.querySelector('#result')
 
-console.log({
-    h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
-
-
-h1.innerHTML = 'Nuevo titulo';
-h1.innerText = 'Titulo con innerText'
-
-// h1.getAttribute('verde');
-// h1.setAttribute('class', 'rojo')
-h1.setAttribute('id','nuevoId')
-
-h1.classList.add('rojo')
-// h1.classList.remove('rojo')
-
-// h1.classList.toggle('rojo')
-h1.classList.contains('rojo')
-console.log(h1.classList.contains('rojo'));
-
-let img = document.createElement('img')
-img.setAttribute('src', 'https://www.purina.es/sites/default/files/styles/ttt_image_510/public/2024-02/sitesdefaultfilesstylessquare_medium_440x440public2022-06Bengal-Cat.jpg?itok=iVh6G_Ef')
-
-console.log(img);
-
-div.append(img)
-
+function btnOnClick(){
+    let sumaInputs = Number(input1.value) + Number(input2.value)
+    result.innerText = "El resultado es: " + sumaInputs
+    
+    
+}
 
