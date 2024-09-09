43122c4a7db6f299bc6b73e3be23028acadc4b79
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-09-09 02:42:35 -0600

    addEventListener

--- 31 - Curso Practico de JavaScript/app.js
+++ 31 - Curso Practico de JavaScript/app.js
@@ -1,13 +1,25 @@
-let h1 = document.querySelector('h1')
-let input1 = document.querySelector('#calculo1')
-let input2 = document.querySelector('#calculo2')
-let btnCalcular = document.querySelector('#btnCalcular')
-let result = document.querySelector('#result')
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+const form = document.querySelector('#form');
 
-function btnOnClick(){
-    let sumaInputs = Number(input1.value) + Number(input2.value)
-    result.innerText = "El resultado es: " + sumaInputs
-    
-    
+form.addEventListener('submit', sumarInputValues)
+
+function sumarInputValues(event) {
+    console.log({ event });
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
 }
+
+// btn.addEventListener('submit', sumarInputValues)
+
+// function sumarInputValues(event) {
+//   console.log({event});
+//   event.preventDefault();
+//   const sumaInputs = Number(input1.value) + Number(input2.value);
+//   pResult.innerText = "Resultado: " + sumaInputs;
+// }
 
