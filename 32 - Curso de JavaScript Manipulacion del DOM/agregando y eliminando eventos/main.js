db087f093cfb768346f183ad37ae6e7be9a21640
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-09-18 15:28:42 -0600

    Objeto evento

--- 32 - Curso de JavaScript Manipulacion del DOM/agregando y eliminando eventos/main.js
+++ 32 - Curso de JavaScript Manipulacion del DOM/agregando y eliminando eventos/main.js
@@ -1,33 +1,8 @@
-const container = document.querySelector('.container')
 const button = document.querySelector('button')
 
-container.addEventListener("mouseover", () => {
-    container.style.backgroundColor = "blue";
-})
-
-container.addEventListener("mouseout", ()=>{
-    container.style.backgroundColor = "red";
-})
-
-// button.addEventListener('click',()=>{
-//     alert("Button clicked!!!")
-// })
-
-// retirar eventos
-
-// tendriamos que guardarlo en una funcion para decirle que queremos retirarlo
-
-const buttonClickCallback = ()=> {
-    alert('Button Clicked!!!')
-    console.log('Hola');
+const buttonClick = (event) => {
+    console.log(event.target.textContent);
     
 }
 
-button.addEventListener('click', buttonClickCallback)
-
-// como retiramos este evento
-// button.removeEventListener ('click',buttonClickCallback)
-
-setTimeout(()=>{
-    button.removeEventListener('click',buttonClickCallback)
-},2000)
+button.addEventListener('click', buttonClick)
