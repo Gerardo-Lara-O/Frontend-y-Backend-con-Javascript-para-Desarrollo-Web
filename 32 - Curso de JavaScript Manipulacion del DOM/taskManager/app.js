f112d70cf50cf5fe3d9cba80b79489bfb30f435a
Gerardo Lara <gerardo.lara_sic@tesco.edu.mx>
2024-09-18 21:55:39 -0600

    Agregamos el evento e imprimimos la tarea

--- 32 - Curso de JavaScript Manipulacion del DOM/taskManager/app.js
+++ 32 - Curso de JavaScript Manipulacion del DOM/taskManager/app.js
@@ -9,5 +9,23 @@ taskForm.addEventListener('submit',(event)=>{ // mandamos a llamar al evento par
     const taskInput = document.getElementById('task-input')
     const task = taskInput.value
     console.log(task);
-    
+    if(task){
+        taskList.append(createTaskElement(task))
+        taskInput.value = ''
+    }
 })
+
+function createTaskElement(task){
+    const li = document.createElement('li')
+    li.textContent = task
+    li.append(createButton('❌','delete-btn'),
+    createButton('✏️','edit-btn'))
+    return li
+}
+
+function createButton(text,className){
+    const btn = document.createElement('span')
+    btn.textContent = text
+    btn.className = className
+    return btn
+}
